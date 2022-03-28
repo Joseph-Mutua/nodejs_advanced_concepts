e4bed0e8daef995ea643bd655f5a179706872165
Joseph Mutua <Mutuaj793@gmail.com>
2022-03-29 01:12:46 +0300

    Working with the cluster module

---
+++ node_performance/index.js
@@ -0,0 +1,22 @@
+const cluster = require("cluster");
+
+if (cluster.isMaster) {
+  // Cause index.js to be executed again but in child mode
+  cluster.fork();
+} else {
+  //Child, acts like a server and does nothing else
+  const express = require("express");
+  const app = express();
+
+  function doWork(duration) {
+    const start = Date.now();
+    while (Date.now() - start < duration) {}
+  }
+
+  app.get("/", (req, res) => {
+    doWork(5000);
+    res.send("Hi There!");
+  });
+
+  app.listen(3000);
+}
