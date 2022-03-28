6d47b74d6e0f2d5416c2782a1cef8247ed594dc3
Joseph Mutua <Mutuaj793@gmail.com>
2022-03-28 23:10:39 +0300

    Event loop ticks

--- node_internals/event_loop.js
+++ node_internals/event_loop.js
@@ -1,3 +1,5 @@
+// THE EVENT LOOP IMPLEMENTATION
+
 // node myFile.js
 const pendingTimers = []
 const pendingOSTasks = []
@@ -14,8 +16,28 @@ function shouldContinue() {
     return pendingTimers.length || pendingOSTasks.length || pendingOperations.length
 }
 
+
+// THE EVENT LOOP TICKS
+
 // Entire body executes in one "tick"
 while(shouldContinue()){
+    // 1. Node looks at pendingTimers and sees if any functions need to be called:
+    //      -setTimeOut, setInterval
+    
+    // 2. Node looks at pendingOSTasks  and pendingOperations and calls relevant callbacks
+   
+    // 3. Pause execution. Continue when ...
+    //     - A new pendingOSTaks is done
+    //     - A pendingOperation is done
+    //     - A timer is aboout to complete
+
+    // 4. Look at pendingTimers. Call any settImmediate
+
+    // 5. Handle any 'close' event
+
+
+
+
 
 }
 
