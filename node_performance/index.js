bdb05700469b35e1956ce684b3ca8241603ffb0c
Joseph Mutua <Mutuaj793@gmail.com>
2022-03-29 12:58:33 +0300

    Benchmarking workers

--- node_performance/index.js
+++ node_performance/index.js
@@ -34,11 +34,27 @@
 const express = require("express");
 const crypto = require("crypto");
 const app = express();
+const Worker = require("webworker-threads").Worker;
 
 app.get("/", (req, res) => {
-  crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
-    res.send("Hi There!");
+  const worker = new Worker(function () {
+    this.onmessage = function () {
+      let counter = 0;
+
+      while (counter < 1e9) {
+        counter++;
+      }
+
+      postMessage(counter);
+    };
   });
+
+  worker.onmessage = function (message) {
+    console.log(message.data);
+    res.send(message.data);
+  };
+
+  worker.postMessage();
 });
 
 app.get("/fast", (req, res) => {
