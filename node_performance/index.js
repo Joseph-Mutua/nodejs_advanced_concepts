db43c626ca48392d2e78beecb833aa1dbdb7bda8
Joseph Mutua <Mutuaj793@gmail.com>
2022-03-29 12:34:35 +0300

    Install webworker threads

--- node_performance/index.js
+++ node_performance/index.js
@@ -1,30 +1,48 @@
-const cluster = require("cluster");
+// process.env.UV_THREADPOOL_SIZE =1
+// const cluster = require("cluster");
 
-if (cluster.isMaster) {
-  // Cause index.js to be executed again but in child mode
-  cluster.fork(); //Starts one child node
-//   cluster.fork();
-//   cluster.fork();
-//   cluster.fork();
-//   cluster.fork();
-} else {
-  //Child, acts like a server and does nothing else
-  const express = require("express");
-  const app = express();
+// if (cluster.isMaster) {
+// Cause index.js to be executed again but in child mode
+//   cluster.fork(); //Starts one child node //Every child has its own threadpool with 4 threads
+//  NB: INCREASING THE NUMBER OF CHILDREN BEYOND THE NUMBER OF PHYSICAL CORES
+//      HAS DIMINISHING RETURNS ON THE PERFORMANCE OF THE PROGRAM
 
-  function doWork(duration) {
-    const start = Date.now();
-    while (Date.now() - start < duration) {}
-  }
+//   cluster.fork();
+//   cluster.fork();
+//   cluster.fork();
+//   cluster.fork();
+// } else {
+//Child, acts like a server and does nothing else
+//   const express = require("express");
+//   const crypto = require("crypto");
+//   const app = express();
 
-  app.get("/", (req, res) => {
-    doWork(5000);
+//   app.get("/", (req, res) => {
+//     crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+//       res.send("Hi There!");
+//     });
+//   });
+
+//   app.get("/fast", (req, res) => {
+//     res.send("This was fast!");
+//   });
+
+//   app.listen(3000);
+// }
+
+// USING PM2 FOR CLUSTER MANAGEMENT
+const express = require("express");
+const crypto = require("crypto");
+const app = express();
+
+app.get("/", (req, res) => {
+  crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
     res.send("Hi There!");
   });
+});
 
-  app.get("/fast", (req, res) => {
-    res.send("This was fast!");
-  });
+app.get("/fast", (req, res) => {
+  res.send("This was fast!");
+});
 
-  app.listen(3000);
-}
+app.listen(3000);
